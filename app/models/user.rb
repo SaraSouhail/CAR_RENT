d24d4898f2751b0c7bed5ed76af4466ff69f62ae
AnassNajam <contact.anassnajam@gmail.com>
2024-01-23 16:59:32 +0100

    User controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :client
+  has_one :owner
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
