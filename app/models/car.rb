d4eff3ffe9e5b7943944936d623adbd2e3cc4bd7
AnassNajam <contact.anassnajam@gmail.com>
2024-01-29 15:52:37 +0100

    We are done

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :owner
+  has_one_attached :photo
   has_many :clients, through: :bookings
   validates :model, :brand, :price, presence: true
   validates :price, numericality: { greater_than: 150 }
